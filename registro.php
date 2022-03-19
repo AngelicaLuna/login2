566f6c913753ca3614037c4a204ad3e10b1c8ced
AngelicaLuna <50463124+AngelicaLuna@users.noreply.github.com>
2022-03-19 09:10:44 -0600

    Add files via upload

--- registro.php
+++ registro.php
@@ -1,21 +1,3 @@
 <?php
 include('config.php');
 session_start();
-
-$username = $_POST['usuario']
-$password = $_POST['password']
-
-$query = $conn ->prepare ("SELECT * FROM usuarios WHERE username = :username");
-$query ->bindParam("username, $username, PDO::PARAM_STR");
-$query -> execute();
-
-$result = $query-> fetch(PDO::FETCH_ASSOC);
-if (!$result){
-    echo 'Usuario o password es incorrecto';
-}else {
-    if(password_verify($password, $result['PASSWORD'])){
-        echo 'Felicitaciones, bienvenido';
-    }else{.
-    echo 'Error al iniciar, intente nuevamente';
-}
-}
